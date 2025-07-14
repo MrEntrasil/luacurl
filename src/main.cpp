1dc5df6065a4986d30272956477b3210c9a24c79
MrEntrasil <mrentrasil@gmail.com>
2025-07-14 01:08:16 +0000

    added: luacurl_downloadfile

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@ extern "C" {
 #include <lua5.4/lua.h>
 #include <lua5.4/lauxlib.h>
 }
+
 #include <curl/curl.h>
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 size_t write_cb(void* contents, size_t size, size_t nmemb, void* userp) {
 	((std::ostringstream*)userp)->write((char*)contents, size*nmemb);
@@ -16,6 +18,9 @@ int luacurl_request(lua_State* L) {
 	const char* url = luaL_checkstring(L, 1);
 	const char* method = luaL_optstring(L, 2, "GET");
 	const char* body = luaL_optstring(L, 3, "");
+	std::ostringstream res;
+	struct curl_slist* headers;
+	CURLcode res_code;
 	CURL* curl = curl_easy_init();
 
 	if (!curl) {
@@ -23,16 +28,16 @@ int luacurl_request(lua_State* L) {
 		lua_pushstring(L, "curl initializing fail");
 		return 0;
 	}
-	std::ostringstream res;
+	
 	curl_easy_setopt(curl, CURLOPT_URL, url);
 	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_cb);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res);
+	
 	if (body && strlen(body) > 0) {
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body);
 	}
 
-	struct curl_slist* headers = nullptr;
 	if (lua_istable(L, 4)) {
 		lua_pushnil(L);
 		while (lua_next(L, 4) != 0) {
@@ -43,7 +48,7 @@ int luacurl_request(lua_State* L) {
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 	}
 
-	CURLcode res_code = curl_easy_perform(curl);
+	res_code = curl_easy_perform(curl);
 	if (headers) {
 		curl_slist_free_all(headers);
 	}
@@ -59,7 +64,30 @@ int luacurl_request(lua_State* L) {
 	return 1;
 }
 
+int luacurl_downloadfile(lua_State* L) {
+	const char* url = luaL_checkstring(L,1);
+	const char* filename = luaL_checkstring(L,2);
+	std::ofstream outfile(filename, std::ios::binary);
+	CURLcode res;
+	CURL* curl = curl_easy_init();
+
+	if (!outfile.is_open() || !curl) {
+		lua_pushnil(L);
+		return 0;
+	}
+
+	curl_easy_setopt(curl,CURLOPT_URL,url);
+	curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,&write_cb);
+	curl_easy_setopt(curl,CURLOPT_WRITEDATA,&outfile);
+	res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+
+	outfile.close();
+	return 1;
+}
+
 extern "C" int luaopen_luacurl(lua_State* L) {
 	lua_register(L, "luacurl_request", luacurl_request);
+	lua_register(L, "luacurl_downloadfile", luacurl_downloadfile);
 	return 0;
 }
--- test.lua
+++ test.lua
@@ -1,10 +1,9 @@
 require("luacurl")
-
+--[[
 print(luacurl_request(
 	"https://example.com",
-	"GET",
-	nil,
-	{
-		"User-Agent: Mozilla/5.0 (Linux; Android 14 AppleWebKit/537.36 (KHTML, like Gecko) brave/136.0.7103.60 Mobile Safari/537.36"
-	}
+	"GET"
 ))
+--]]
+
+luacurl_downloadfile("https://raw.githubusercontent.com/MrEntrasil/luacurl/refs/heads/main/src/main.cpp", "test");
